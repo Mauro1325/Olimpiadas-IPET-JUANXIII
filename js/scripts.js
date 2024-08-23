f12b872a87dc145a02554a926f3cd80b5f696778
Mauro1325 <122283051+Mauro1325@users.noreply.github.com>
2024-08-23 06:14:31 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const secciones = document.querySelectorAll('.contenido');
-
-    secciones.forEach(seccion => {
-        const header = seccion.querySelector('h2');
-        const contenido = seccion.querySelector('.contenido-desplegable');
-
-        header.addEventListener('click', function() {
-            contenido.style.display = contenido.style.display === 'none' || contenido.style.display === '' ? 'block' : 'none';
-        });
-    });
-});
+function expandCard(card, contentId) {
+    card.classList.add("hidden");
+  
+    const activeContent = document.querySelector(".expanded-content.active");
+    if (activeContent) {
+      activeContent.classList.remove("active");
+  
+      const prevCardId = activeContent
+        .getAttribute("id")
+        .replace("content", "");
+      const prevCard = document.querySelector(
+        `.interactive-card[onclick="expandCard(this, 'content${prevCardId}')"]`
+      );
+      if (prevCard) {
+        prevCard.classList.remove("hidden");
+      }
+    }
+  
+    const newContent = document.getElementById(contentId);
+    newContent.classList.add("active");
+  
+    const cardList = document.querySelector(".card-list");
+    cardList.classList.add("vertical");
+  }
+  
+  function collapseCard(contentId, contentElement) {
+    const content = document.getElementById(contentId);
+    content.classList.remove("active");
+  
+    const cardId = contentId.replace("content", "");
+    const card = document.querySelector(
+      `.interactive-card[onclick="expandCard(this, 'content${cardId}')"]`
+    );
+    if (card) {
+      card.classList.remove("hidden");
+    }
+  
+    const cardList = document.querySelector(".card-list");
+    cardList.classList.remove("vertical");
+  }
+  
